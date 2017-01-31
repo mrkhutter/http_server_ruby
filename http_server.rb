20a8c7dc9ffa5e9bdc36d87b71b131b042628092
Mark Hutter <mrkhutter@gmail.com>
2017-01-31 10:01:10 -0600

    use rack app to create status, headers, and body

--- http_server.rb
+++ http_server.rb
@@ -1,14 +1,28 @@
 require 'socket'
+
+app = Proc.new do
+  ['200', {'Content-Type' => 'text/html'}, ["Hello world! The time is #{Time.now}"]]
+end
+
 server = TCPServer.new 5678
 
 while session = server.accept
   request = session.gets
   puts request
 
-  session.print "HTTP/1.1 200\r\n"
-  session.print "Content-Type: text/html\r\n"
+  status, headers, body = app.call({})
+
+  session.print "HTTP/1.1 #{status}\r\n"
+
+  headers.each do |k, v|
+    session.print "#{k}: #{v}\r\n"
+  end
+
   session.print "\r\n"
-  session.print "Hello world! The time is #{Time.now}"
+
+  body.each do |part|
+    session.print part
+  end
 
   session.close
 end
